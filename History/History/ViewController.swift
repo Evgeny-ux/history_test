5f0e53c5dae75496574b256e835b14ca9b30d56c
Kamaev Evgenij <e.kamaev@corp.mail.ru>
2022-05-30 09:54:17 +0300

    Created project

---
+++ History/History/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  History
+//
+//  Created by Evgeny Kamaev on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
